1a8356c734b86727fe766f41f9437536d5016229
ShodanMaster <akshaykrishnan922@gmail.com>
2025-11-29 05:39:37 +0530

    Staring with Component Communication

---
+++ Vue Component Demo/ComponentCommunication/src/App.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <h1>{{ message }}</h1>
+  <ContactUs name="John Doe" phone="9876543120"></ContactUs>
+
+</template>
+
+<script setup>
+import ContactUs from './components/ContactUs.vue'
+const message = "Hello World!";
+
+</script>
+<style scoped></style>
---
+++ Vue Component Demo/ComponentCommunication/src/components/ContactUs.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-info rounded p-2 m-2">
+        <h3>Name: {{ name }}</h3>
+        <p>Email: {{ email }}</p>
+        <p>Phone: {{ phone }}</p>
+    </div>
+</template>
+<script setup>
+import {ref, defineProps} from 'vue'; 
+const email = ref('contact@example.com');
+
+const props = defineProps({
+    name: String,
+    phone: String,
+});
+
+</script>
