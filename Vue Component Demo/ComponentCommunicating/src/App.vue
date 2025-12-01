48dc065affff7f0d79a102cfc9c285fb998bcefc
ShodanMaster <akshaykrishnan922@gmail.com>
2025-12-02 00:27:22 +0530

    Emit and more

---
+++ Vue Component Demo/ComponentCommunicating/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="row">
+        <div class="col-12" v-for="contact in contacts" :key="contact.id">
+            <Contact 
+                :name="contact.name"
+                :phone="contact.phone"
+                :email="contact.email"
+                :isFav="contact.isFav"
+                :message="contact.message"
+                @updateFav="onUpdateEvent(contact, $event)"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import Contact from './components/Contact.vue'
+
+const contacts = reactive([
+    {
+        id: 1,
+        name: "Akshay",
+        phone: 9876543210,
+        email: "akshay@example.com",
+        isFav: false,
+        message: "",
+    },
+    {
+        id: 2,
+        name: "Shodan",
+        phone: 98765432101,
+        isFav: true,
+        message: "",
+    },
+    {
+        id: 3,
+        name: "Kannan",
+        phone: 9876543210,
+        email: "kannan@example.com",
+        isFav: false,
+        message: "",
+    },
+])
+
+function onUpdateEvent(contact, message) {
+    contact.isFav = !contact.isFav
+    contact.message = contact.isFav ? message : ""
+}
+</script>
---
+++ Vue Component Demo/ComponentCommunicating/src/components/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="rounded p-2 mb-3" :class="isFav ? 'bg-info' : 'bg-warning'">
+        <p>Name: {{ name }}</p>
+        <p>Phone: {{ phone }}</p>
+        <p>Email: {{ email }}</p>
+
+        <button class="btn" :class="[isFav ? 'btn-warning' : 'btn-success']" @click="toggleFav">
+            {{ isFav ? 'Remove' : 'Add' }}
+        </button>
+        <div 
+            class="bg-danger text-white rounded m-1 p-1" 
+            id="message" 
+            v-if="isFav && message"
+        >
+            {{ message }}
+        </div>
+
+    </div>
+</template>
+<script setup>
+
+const props = defineProps({
+    name: {type: String, required: true},
+    phone: Number,
+    email: {type: String, required: false, default: 'nil'},
+    isFav: {type: Boolean, default: false},
+    message: String,
+});
+
+const emit = defineEmits(["updateFav"]);
+
+function toggleFav(){
+    console.log('clicked');
+    emit("updateFav", "This is your Favourite Contact");
+}
+</script>
